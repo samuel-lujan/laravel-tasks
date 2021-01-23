aa5d159baf94784914738442e613feeb5c462367
Samuel Lujan <samuel_lujan@hotmail.com>
2021-01-23 08:20:14 -0300

    Adicionando Template adminlte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/',     [App\Http\Controllers\HomeController::class, 'index']);
+
+Auth::routes();
